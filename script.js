3ab8bd6f1a4dea3d0b4dfd41964f6714247f6f07
davenportnw <davenportnw@gmail.com>
2022-05-17 22:06:09 -0500

    Reload functiong

--- script.js
+++ script.js
@@ -1,62 +1,53 @@
 
-$(document).ready(function() {   
+function numberRange(start, end) {
+    //fill an array with sequential numbers
+    return new Array(end - start).fill().map((e, i) => i + start);
+}
+    
+function pickedPhotos() {
+    //create an array of images I can use
+    let images = numberRange(1, 31).map((e) => "photos/img_" + e + ".png");
+    
+    //shuffle images
+    let shuffledImages = shuffle(images);
 
-    var clickedTime = 0;
+    //pick first 20 
+    return shuffledImages.slice(0, 20);
+}
+
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+$(document).ready(function() {
+     //create game cards 
+     function createGameCard() {
+        let chosenPhotos = pickedPhotos();
+        for(i=0; i<chosenPhotos.length; i++) {
+            $('#card').append('<div class="col"> <img src= "' + chosenPhotos[i] + '" ' + ' class="img-thumbnail" alt="..."></div>');   
+        }
+    }
+
     $("#btn").click(function(){
-        function numberRange(start, end) {
-            //fill an array with sequential numbers
-            return new Array(end - start).fill().map((e, i) => i + start);
-        }
-            
-        function pickedPhotos() {
-            //create an array of images I can use
-            let images = numberRange(1, 31).map((e) => "photos/img_" + e + ".png");
-            
-            //shuffle images
-            let shuffledImages = shuffle(images);
-    
-            //pick first 20 
-            return shuffledImages.slice(0, 20);
-        }
-
-        //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-        function shuffle(array) {
-            let currentIndex = array.length,  randomIndex;
-          
-            // While there remain elements to shuffle.
-            while (currentIndex != 0) {
-          
-              // Pick a remaining element.
-              randomIndex = Math.floor(Math.random() * currentIndex);
-              currentIndex--;
-          
-              // And swap it with the current element.
-              [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
-            }
-          
-            return array;
-          }
-    
-        //create game cards 
-        function createGameCard() {
-            let chosenPhotos = pickedPhotos();
-            for(i=0; i<chosenPhotos.length; i++) {
-                $('#card').append('<div class="col"> <img src= "' + chosenPhotos[i] + '" ' + ' class="img-thumbnail" alt="..."></div>');   
-            }
-        }
-        clickedTime++;
-        console.log("clickedTime: ", clickedTime);
-
-        if(clickedTime > 1) {
-            location.reload(false);
-        } else {
-            createGameCard();
-        }
-        
+       location.reload();
     });
 
-
+    createGameCard();
 });
 
 
